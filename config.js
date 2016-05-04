79c0b507ff0fa65547844ad488705a4303ac411c
hrushikesh09 <hrushikeshshinde11@yahoo.com>
2016-05-04 10:59:27 +0530

    changed webhost again

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {};
 
 config.db = {};
 
-config.webhost = 'https://hs-url-shortener.herokuapp.com';
+config.webhost = 'https://hs-url-shortener.herokuapp.com/';
 
 config.db.host = 'localhost';
 config.db.name = 'url-shortener';
